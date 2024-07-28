4078c8cf84aae98dd5439110de48f084db7fb04c
Guilherme de Lima <guilhermelimasantosn7@gmail.com>
2024-07-28 20:08:01 -0300

    algumas correções e botoes

---
+++ script.js
@@ -0,0 +1,52 @@
+const imagens = document.querySelectorAll('.col5 img');
+const overlay = document.getElementById('overlay');
+const controls = document.getElementById('controls');
+const prevBtn = document.getElementById('prevBtn');
+const closeBtn = document.getElementById('closeBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentImageIndex = 0;
+
+function showImage(index) {
+    imagens.forEach(imagem => imagem.classList.remove('expanded'));
+    imagens[index].classList.add('expanded');
+    overlay.classList.add('active');
+    controls.classList.add('active');
+}
+
+imagens.forEach((imagem, index) => {
+    imagem.addEventListener('click', () => {
+        currentImageIndex = index;
+        showImage(index);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    controls.classList.remove('active');
+    const expandedImage = document.querySelector('.expanded');
+    if (expandedImage) {
+        expandedImage.classList.remove('expanded');
+    }
+});
+
+prevBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    currentImageIndex = (currentImageIndex - 1 + imagens.length) % imagens.length;
+    showImage(currentImageIndex);
+});
+
+nextBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    currentImageIndex = (currentImageIndex + 1) % imagens.length;
+    showImage(currentImageIndex);
+});
+
+closeBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    overlay.classList.remove('active');
+    controls.classList.remove('active');
+    const expandedImage = document.querySelector('.expanded');
+    if (expandedImage) {
+        expandedImage.classList.remove('expanded');
+    }
+});
